fdd9f0d1bf19add73960607c563dbd7caa631213
Roman Tolstopyatov <r.tolstopyatov@gmail.com>
2018-04-13 23:58:03 +0300

    get coordinates and weather JSON

--- src/index.js
+++ src/index.js
@@ -1,5 +1,34 @@
+function httpGet(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url, true);
+    xhr.onload = function() {
+      if (this.status == 200) {
+        resolve(this.response);
+      } else {
+        const err = new Error(this.statusText);
+        err.code = this.status;
+        reject(err);
+      }
+    };
+    xhr.onerror = () => { reject(new Error('Network Error')); };
+    xhr.send();
+  });
+}
+
 ymaps.ready(() => {
   let placemark;
+
+  httpGet('https://freegeoip.net/json/')
+    .then(
+      (res) => {
+        console.log(res);
+        const {latitude, longitude, city} = JSON.parse(res);
+        console.log(city);
+        return httpGet(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${config.owmid}`)
+      },
+      (err) => {console.log(err);}
+    ).then((res) => {console.log(res);});
 
   const myMap = new ymaps.Map('map', {
     center: [55.76, 99.64],
