ed053686d96ca26a3d64aeb1e9befb8e402f5e2f
Roman Tolstopyatov <r.tolstopyatov@gmail.com>
2018-04-13 14:16:09 +0300

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,109 @@
+const gulp = require('gulp');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const watch = require('gulp-watch');
+const sass = require('gulp-sass');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const eslint = require('gulp-eslint');;
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
+const browserSync = require('browser-sync');
+const source = require('vinyl-source-stream');
+const htmlmin = require('gulp-htmlmin');
+const buffer = require('vinyl-buffer');
+
+gulp.task('sass', () => {
+  gulp.src('./src/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./build'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('js-lint', () => {
+  gulp.src('./src/**.js')
+    .pipe(eslint())
+    .pipe(eslint.format())  
+    .pipe(eslint.failAfterError());
+});
+
+gulp.task('js-build', () => {
+  return browserify({
+    entries: './src/index.js',
+    debug: true,
+    extensions: ['es6'],
+    transform: [babelify.configure({
+      presets: ["es2015"]
+    })]
+  })
+    .bundle()
+    .pipe(source('index.js'))
+    .pipe(gulp.dest('./build/'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('browser-sync', () => {
+  browserSync({
+    server: {baseDir: './build/'},
+    port: 8080,
+    open: true
+  });
+});
+
+gulp.task('html', () => {
+  gulp.src(['./src/**/*.html'])
+    .pipe(gulp.dest('./build'))
+    .pipe(browserSync.reload({stream: true}));
+});
+
+gulp.task('watch', () => {
+  gulp.watch('./src/**/*.js', ['js-lint', 'js-build']);
+  gulp.watch('./src/**/*.scss', ['sass']);
+  gulp.watch('./**/*.html', ['html']);
+});
+
+gulp.task('default', ['js-lint', 'js-build', 'sass', 'html', 'watch', 'browser-sync']);
+
+gulp.task('sass-dist', () => {
+  gulp.src('./src/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(autoprefixer({
+      browsers: ['last 2 versions'],
+      cascade: false
+    }))
+    .pipe(cleanCSS({compatibility: 'ie8'}))
+    .pipe(concat('style.css'))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('js-dist', () => {
+  return browserify({
+    entries: './src/index.js',
+    debug: false,
+    extensions: ['es6'],
+    transform: [babelify.configure({
+      presets: ["es2015"]
+    })]
+  })
+    .bundle()
+    .pipe(source('index.js'))
+    .pipe(buffer())
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/'));
+});
+
+gulp.task('html-dist', () => {
+  gulp.src(['./src/**/*.html'])
+    .pipe(htmlmin({
+      collapseWhitespace: true,
+      removeComments: true
+    }))
+    .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('dist', ['js-dist', 'sass-dist', 'html-dist']);
---
+++ src/index.js
@@ -0,0 +1,16 @@
+let myMap = 1;
+let myPlacemark;
+
+ymaps.ready(() => {
+  myMap = new ymaps.Map('map', {
+    center: [55.76, 37.64],
+    zoom: 7,
+  });
+
+  myPlacemark = new ymaps.Placemark([55.76, 37.64], {
+    hintContent: 'Москва!',
+    balloonContent: 'Столица России',
+  });
+
+  myMap.geoObjects.add(myPlacemark);
+});
